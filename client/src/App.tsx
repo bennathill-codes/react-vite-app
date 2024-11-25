7457280dd1f92ac40559e36387e8d8a05a8a1198
Benjamin Hill <bennathill@benjamins-mbp.mynetworksettings.com>
2024-11-25 11:02:36 -0800

    RVA-103: Create frontend pages and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import axios from 'axios'; 
-import { useState, useEffect } from 'react';
-import { getUsers, createUser } from './api'
 import './App.css'
 
 axios.defaults.baseURL = "http://localhost:3000";
@@ -8,32 +6,8 @@ axios.defaults.withCredentials = true;
 
 function App() {
 
-  const [users, setUsers] = useState();
-
-  useEffect(() => {
-    async function loadAllUsers() {
-      const data = await getUsers();
-
-      if (data) {
-        setUsers(data);
-      }
-    }
-    loadAllUsers();
-  }, [])
-
-  function createNewUser() {
-    const userObject = {
-      email: "test3@test.com",
-      password: "testaccount",
-    }
-    createUser(userObject);
-  }
-
   return (
     <>
-      {/* test api on frontend */}
-      <button onClick={(createNewUser)}>create user</button>
-      {JSON.stringify(users)}
     </>
   )
 }
