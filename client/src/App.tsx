fff93b646426276fa4fb74446da13748200c0a40
Benjamin Hill <bennathill@benjamins-mbp.mynetworksettings.com>
2024-11-25 13:08:10 -0800

    Set up page routing in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
-import axios from 'axios'; 
+import axios from 'axios';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Home from '../pages/Home';
+import Search from '../pages/Search';
+import About from '../pages/About';
+import Profile from '../pages/Profile';
 import './App.css'
 
 axios.defaults.baseURL = "http://localhost:3000";
@@ -7,8 +14,16 @@ axios.defaults.withCredentials = true;
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
